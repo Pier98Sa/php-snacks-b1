88ba7619abfc08f9aab7fb9fc01a36221cbef5ff
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-03-10 17:15:27 +0100

    Snack5 completato

--- snack5/index.php
+++ snack5/index.php
@@ -15,13 +15,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dividere un testo</title>
 </head>
 <body>
     <h1>Testo:</h1>
     <p><?php echo $testo ?></p>
 
     <h1>Testo diviso:</h1>
-    <p><?php ?></p>
+    <p><?php for($i=0; $i < count($testoDiviso); $i++){echo $testoDiviso[$i] . '<br>';}?></p>
 </body>
 </html>
