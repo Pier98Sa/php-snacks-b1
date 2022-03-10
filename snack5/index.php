5085672b99ecca50077f539d9db8f0187e869535
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-03-10 17:07:07 +0100

    snack5

---
+++ snack5/index.php
@@ -0,0 +1,27 @@
+<?php
+    $testo = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed sint, ipsum asperiores impedit doloremque quos officiis quam possimus iste ratione veniam veritatis iure cumque. Explicabo necessitatibus omnis nemo blanditiis quis!';
+
+    $testoDiviso = explode(".",$testo);
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Testo:</h1>
+    <p><?php echo $testo ?></p>
+
+    <h1>Testo diviso:</h1>
+    <p><?php ?></p>
+</body>
+</html>
