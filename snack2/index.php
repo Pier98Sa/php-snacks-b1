41c3c94a80dfacc34bf23736cbfc874a84f72c52
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-03-10 17:47:14 +0100

    Snack 2 completato

--- snack2/index.php
+++ snack2/index.php
@@ -8,8 +8,6 @@
     $nameLength = strlen($name);
 
     //validazione dell'email
-    $validEmail ;
-
     if (strpos($email, '@') !== false && strpos($email, '.') !== false) {
         $validEmail = true;
     } else {
@@ -17,12 +15,9 @@
     }
 
     //validazione del dato age
-
     $validAge = is_numeric($age);
 
     //stampa dell'Esito
-
-    $esito ;
     if( ($nameLength > 3) && ($validEmail) && ($validAge)){
         $esito = "Accesso riuscito";
     }else{
@@ -30,14 +25,13 @@
     }
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Validazione</title>
 </head>
 <body>
     <h1><?php echo $esito; ?> </h1>
